7da1fe77cd1a983a44b0380f3ed890e818ccabdd
titus <me@titusio.net>
2026-05-05 15:46:57 +0200

    corellian: add autoupdate webhook

--- vps/auto-update.nix
+++ vps/auto-update.nix
@@ -11,4 +11,27 @@
     dates = "04:00 UTC";
     randomizedDelaySec = "45min";
   };
+
+  services.webhook = {
+    enable = true;
+    hooks = {
+      nixos-rebuild = {
+        execute-command = "/run/current-system/sw/bin/nixos-rebuild";
+        pass-arguments-to-command = [
+          {
+            source = "string";
+            name = "switch";
+          }
+          {
+            source = "string";
+            name = "--flake";
+          }
+          {
+            source = "string";
+            name = "git+ssh://git@github.com/titusio/homelab#corellian";
+          }
+        ];
+      };
+    };
+  };
 }
