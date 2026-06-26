031e00d14c27b1070e1261af8ca30bb6ec11433b
titus <me@titusio.net>
2026-06-26 15:40:56 +0200

    Refactor nix ssh keys

--- vps/hosts/corellian-run.nix
+++ vps/hosts/corellian-run.nix
@@ -3,7 +3,9 @@
   config,
   lib,
   ...
-}: {
+}: let
+  sshKeys = import ../ssh-keys.nix;
+in {
   vps = {
     firewall.enable = true;
     autoUpdate.enable = true;
@@ -25,22 +27,14 @@
 
   users.users.root = {
     hashedPassword = "$y$j9T$sm19yaRkje0AOBwW/pTRt.$MntXjJC6P.rGgdG64rvTOKATAxDoggPXyIADTBFx8B.";
-    openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBZAMR7ANTdIik3dYOG/oDHKzE7GhKjHSRwuhcWWtSUM titus@nixos"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINRkA6GHUlDJRacyT9dx0bbUdu6MVyhgRGe3QLb6UhWn titus@coruscant"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFHEm75mX3Tsdt7tksWXcvu3RGGJvIj9xi+ZC/jkqx2c titus@Tituss-MacBook-Pro.local"
-    ];
+    openssh.authorizedKeys.keys = sshKeys;
   };
 
   users.users.titus = {
     isNormalUser = true;
     description = "titus";
     extraGroups = ["networkmanager" "wheel"];
-    openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBZAMR7ANTdIik3dYOG/oDHKzE7GhKjHSRwuhcWWtSUM titus@nixos"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINRkA6GHUlDJRacyT9dx0bbUdu6MVyhgRGe3QLb6UhWn titus@coruscant"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFHEm75mX3Tsdt7tksWXcvu3RGGJvIj9xi+ZC/jkqx2c titus@Tituss-MacBook-Pro.local"
-    ];
+    openssh.authorizedKeys.keys = sshKeys;
     hashedPassword = "$y$j9T$b4Dv7lAK98k1KzH7Ef1QM.$79C6YqFZCeA9.Zz5e6uO2TKmSjzqIttFDv6wbIDSMm9";
   };
 
---
+++ vps/ssh-keys.nix
@@ -0,0 +1,4 @@
+[
+  "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBZAMR7ANTdIik3dYOG/oDHKzE7GhKjHSRwuhcWWtSUM titus@nixos"
+  "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFHEm75mX3Tsdt7tksWXcvu3RGGJvIj9xi+ZC/jkqx2c titus@Tituss-MacBook-Pro.local"
+]
