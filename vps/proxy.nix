f8c24615426dc58bceb7fc69f587d5befc500a03
titus <me@titusio.net>
2026-08-04 07:29:57 +0200

    Add external proxy to endor

---
+++ vps/proxy.nix
@@ -0,0 +1,54 @@
+{
+  config,
+  lib,
+  ...
+}: let
+  cfg = config.vps.proxy;
+  domain = "huebie.family";
+  vhosts = [
+    {
+      domain = domain;
+      subdomain = "paperless";
+      ip = "100.93.235.96";
+      port = 30070;
+    }
+    {
+      domain = domain;
+      subdomain = "immich";
+      ip = "100.93.235.96";
+      port = 30041;
+    }
+    {
+      domain = domain;
+      subdomain = "home";
+      ip = "100.93.235.96";
+      port = 8123;
+    }
+    {
+      domain = domain;
+      subdomain = "nextcloud";
+      ip = "100.93.235.96";
+      port = 30027;
+    }
+    {
+      domain = domain;
+      subdomain = "mail-archive";
+      ip = "100.93.235.96";
+      port = 30315;
+    }
+  ];
+in {
+  options.vps.proxy.enable = lib.mkEnableOption "proxying for my parents";
+
+  config = lib.mkIf cfg.enable {
+    services.caddy.virtualHosts = builtins.listToAttrs (map (v: {
+        name = "${v.subdomain}.${v.domain}";
+        value = {
+          extraConfig = ''
+            reverse_proxy ${v.ip}:${toString v.port}
+          '';
+        };
+      })
+      vhosts);
+  };
+}
